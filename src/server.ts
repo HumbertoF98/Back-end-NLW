3af5440a804fb849805ed2d84704d6dadfff648d
Humberto Filho <humbertofilho5@gmail.com>
2020-08-09 13:34:30 -0400

    enviando primeira resposta

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  const users = [
+    { name: "Diego", age: "25" },
+    { name: "Diego", age: "25" },
+    { name: "Diego", age: "25" },
+  ];
+
+  return response.json(users);
+  console.log('Acessou a rota');
+});
+
+app.listen(3333);
